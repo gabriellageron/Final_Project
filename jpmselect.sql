7c438695ffa1bce1ff67bbc62e2f86da4f5c513f
DHANIDHI <53497205+DHANIDHI@users.noreply.github.com>
2020-02-11 21:38:20 -0500

    Add files via upload

---
+++ jpmselect.sql
@@ -0,0 +1,12 @@
+
+SELECT id, symbol, date, open, high, low, close, adjclose, volume
+	FROM jpmorgan.temp where symbol IN ('JPM', 'TSLA');
+	SELECT id, symbol, date, open, high, low, close, adjclose, volume
+	FROM jpmorgan.temp where symbol = 'JPM';
+	
+	
+	
+	
+	
+	
+	
